37ed3e631818552ae303e2f7630e63b5ae67fb5a
thebriann <kellogg.brian@gmail.com>
2013-12-28 20:09:33 -0600

    Adding new WebAPI solution..

---
+++ Zipsurance2014/Zipsurance2014/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zipsurance2014.WebApiApplication" Language="C#" %>
---
+++ Zipsurance2014/Zipsurance2014/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Zipsurance2014.Startup))]
+
+namespace Zipsurance2014
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
